cbd3070019b429141c6827b289bd1eb49b60e6d6
ares-oykot <soykotislam131415@gmail.com>
2023-10-07 10:53:41 +0600

    added navbar

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const Root = () => {
     return (
-        <div className="max-w-screen-xl mx-auto font-poppins border min-h-[100vh]">
-            <Outlet></Outlet>
+        <div className="mx-auto">
+            <Navbar></Navbar>
+            <div className="max-w-screen-xl mx-auto font-poppins border min-h-[100vh]">
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,50 @@
+import { NavLink } from "react-router-dom";
+import Logo from "../../../assets/logo-removebg-preview.png"
+
 const Navbar = () => {
+    const navLinks = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/registration">Registration</NavLink></li>
+    </>
     return (
         <div>
-            
+            <div>
+                <div className="navbar bg-sky-300">
+                    <div className="navbar-start">
+                        <img className="h-14 hidden lg:inline" src={Logo} alt="" />
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                {navLinks}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="md:mr-12 lg:hidden">
+                        <img className="h-10 md:h-14 lg:hidden" src={Logo} alt="" />
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            {navLinks}
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="" />
+                            </div>
+                        </label>
+                        {/* {
+                            user ? <button onClick={handleSignOut} className=" bg-black text-white px-6 py-2 rounded-sm">Sign Out</button>
+                                :
+                                <Link to="/login"><button className=" bg-black text-white px-6 py-2 rounded-sm">Login</button></Link>
+                        } */}
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
